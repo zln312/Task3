8cb3aab37056476e3a973a2e37d9dc3bd95bda2e
zln312 <42492418+zln312@users.noreply.github.com>
2018-10-16 12:44:36 +0800

    Add files via upload

--- src/main/java/com/utility/Interceptor.java
+++ src/main/java/com/utility/Interceptor.java
@@ -1,10 +1,12 @@
 package com.utility;
 
 import org.apache.log4j.Logger;
+
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
+
 
 @Aspect
 @Component
